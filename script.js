f385698b79d23f986de29463cd5ace5bcbf96aa2
Abdoaref739 <105456212+Abdoaref739@users.noreply.github.com>
2026-06-29 16:57:28 +0300

    added the theme page and made some polishing

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 var celebrationButton = document.getElementById("celebration-button");
 var donationButton = document.getElementById("donation-button");
 var greetingButton = document.getElementById("greeting-cards-button");
-var cheersAudio = new Audio("audios/cheers.mp3")
+var cheersAudio = new Audio("audios/music.mp3")
 celebrationButton.addEventListener("click", confettiRespawn);
 function confettiRespawn(){
     cheersAudio.play();
+    let confetti = setInterval(() =>{
     const jsConfetti = new JSConfetti();
     jsConfetti.addConfetti({
         confettiColors: [
             '#fcc138', '#006363'
         ],
     });
+    time += 1;
+    }, 1000);
+    cheersAudio.addEventListener("ended", function(){
+        clearInterval(confetti);
+    });
 }
+
 var donationSection = document.getElementById("donation-section");
 var greetingSection = document.getElementById("greeting-section");
 donationButton.addEventListener("click", function(){
@@ -23,3 +30,7 @@ greetingButton.addEventListener("click", function(){
     greetingSection.style.display = "flex";
     donationSection.style.display = "none";
 })
+var themesBtn = document.getElementById("themes-btn");
+themesBtn.addEventListener("click", function(){
+    location.href = "./themes.html";
+});
