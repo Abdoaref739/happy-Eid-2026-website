abb2d82fe9a31ae1506f4bf663e67dd92f7cd073
Abdoaref739 <105456212+Abdoaref739@users.noreply.github.com>
2026-07-24 16:43:46 +0300

    removed the payment and emails features for now and fixed some bugs

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 var celebrationButton = document.getElementById("celebration-button");
-var donationButton = document.getElementById("donation-button");
-var greetingButton = document.getElementById("greeting-cards-button");
 var cheersAudio = new Audio("audios/music.mp3")
 celebrationButton.addEventListener("click", confettiRespawn);
 function confettiRespawn(){
@@ -18,19 +16,3 @@ function confettiRespawn(){
         clearInterval(confetti);
     });
 }
-
-var donationSection = document.getElementById("donation-section");
-var greetingSection = document.getElementById("greeting-section");
-donationButton.addEventListener("click", function(){
-    donationSection.style.display = "flex";
-    greetingSection.style.display = "none";
-});
-
-greetingButton.addEventListener("click", function(){
-    greetingSection.style.display = "flex";
-    donationSection.style.display = "none";
-})
-var themesBtn = document.getElementById("themes-btn");
-themesBtn.addEventListener("click", function(){
-    location.href = "./themes.html";
-});
